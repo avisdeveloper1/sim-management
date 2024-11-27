0764d111ce47eca4ba8b22ce7eb3421c6863f745
avisdeveloper1 <109718558+avisdeveloper1@users.noreply.github.com>
2024-11-27 13:13:55 +0530

    Update config.php

--- common/config.php
+++ common/config.php
@@ -1,14 +1,9 @@
 <?php
-class Dbconfig {
-    public function dbConnection(){
-        $db = array("connect" => "", "error" => false, "message" => "");
-        $host = 'localhost';
-        $username = 'root';
-        $password = 'cs123';
-        $database = 'sim_management';
+    $host = 'localhost';
+    $username = 'root';
+    $password = 'cs123';
+    $database = 'sim_management';
         
-        $db = mysqli_connect($host, $username, $password, $database);
-        return $db;
-    }   
-}
+    $db = mysqli_connect($host, $username, $password, $database);
+    return $db;
 ?>
